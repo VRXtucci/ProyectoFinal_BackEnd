e7d346a23030accc4a981b737471d943242b60ce
VRXtucci <tucci.jose023@gmail.com>
2023-11-01 23:39:10 -0400

    modificando tipos de datos en la tabla roles

--- database/migrations/2023_11_02_024323_create_roles_table.php
+++ database/migrations/2023_11_02_024323_create_roles_table.php
@@ -4,21 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-
-    public function up(): void
+class CreateRolesTable extends Migration
+{
+    public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->timestamp('fecha_creacion')->useCurrent();
-            $table->timestamp('fecha_modificacion')->useCurrent();
+            $table->string('nombre')->unique(); // Añade restricción unique
+            $table->bigInteger('fecha_creacion')->unique(); // Añade restricción unique
+            $table->bigInteger('fecha_modificacion')->unique(); // Añade restricción unique
             $table->timestamps();
         });
     }
 
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('roles');
     }
-};
+}
